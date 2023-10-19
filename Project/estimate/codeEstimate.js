261bd89dc4153b3ecd31c5c0a22febd07f439274
mrMikoma <miko.lj.mattila@gmail.com>
2023-10-19 10:43:48 +0300

    Estimation update

--- Project/estimate/codeEstimate.js
+++ Project/estimate/codeEstimate.js
@@ -27,6 +27,12 @@ includeVATbox.addEventListener("click", (event) => {
   updateChart();
 });
 
+// Add new action
+const addActionButton = document.getElementById("add-action");
+addActionButton.addEventListener("click", (event) => {
+  updateConsumptionChart();
+});
+
 const getData = async (source) => {
   const res = await fetch(source);
   const data = await res.json();
@@ -130,33 +136,108 @@ const updateChart = () => {
   tempChartData.datasets[0].values = values;
 
   // Update chart
-  updateConsumptionChart(event);
+  updateConsumptionChart();
 };
 
-const consumptionRanges = document.getElementById("range-wrapper");
-consumptionRanges.addEventListener("change", (event) => {
-  updateConsumptionChart(event);
-});
-
-const updateConsumptionChart = (event) => {
+const updateConsumptionChart = () => {
   // Select data day
   let tempChartData = chartDataToday;
   if (daySelector2.checked) {
     tempChartData = chartDataDayahead;
   }
-  let elem = event.target;
 
-  // Set new values
-  let values = tempChartData.datasets[1].values;
-  values[elem.id] = elem.value;
+  // Get action data
+  let action = document.getElementById("action-select");
+  let startTime = document.getElementById("startTime").value;
+  let endTime = document.getElementById("endTime").value;
 
+  console.log(startTime); // dEBUG
+  console.log(endTime); // dEBUG
+
+  // Parse time
+  let startTimeDate = new Date("2019-01-01T" + startTime + ":00.000+00:00");
+  let endTimeDate = new Date("2019-01-01T" + endTime + ":00.000+00:00");
+  if (startTimeDate >= endTimeDate) {
+    console.log("Not possible!");
+    return;
+  }
+
+  // Calculate start minutes
+  if (startTimeDate.getMinutes() != 0) {
+    startTimeDate.setHours(startTimeDate.getUTCHours() + 1);
+    console.log("start minutes " + (60 - startTimeDate.getMinutes())); // DEbug
+    startTimeDate.setMinutes(0);
+    console.log("updated " + startTimeDate.toTimeString()); // DEbug
+  }
+
+  // Calculate new hourly consumption price (NOT IMPLEMENTED YET)
+  let i = 0;
+  let newConsumptionPrices = [];
+  for (let i = 0; i < 24; i++) {
+    newConsumptionPrices.push("0");
+  }
+
+  while (i <= 30) {
+    // End if no full hours left
+    if (startTimeDate >= endTimeDate) {
+      break;
+    }
+
+    // Calculate hour cost
+    console.log("Alkava tunti: " + (startTimeDate.getHours() - 2));
+    newConsumptionPrices[startTimeDate.getHours() - 3] = 5;
+
+    // Iterate
+    startTimeDate.setHours(startTimeDate.getHours() + 1);
+    console.log(
+      "added one! " +
+        startTimeDate.toTimeString() +
+        " to " +
+        startTimeDate.getUTCHours() +
+        " index:" +
+        i
+    ); // DEbug
+
+    ++i;
+    console.log(i); // Debug
+  }
+
+  // Calculate end minutes  (NOT IMPLEMENTED YET)
+  if (endTimeDate.getMinutes() != 0) {
+    console.log("last minutes " + endTimeDate.getMinutes()); // DEbug
+  }
+
+  // Add new consumption values to existing values
+  let oldConsumption = tempChartData.datasets[1].values;
+  oldConsumption.forEach((value, index) => {
+    oldConsumption[index] =
+      Number(oldConsumption[index]) + Number(newConsumptionPrices[index]);
+  });
   // Update both datasets
-  chartDataToday.datasets[1].values = values;
-  chartDataDayahead.datasets[1].values = values;
+  chartDataToday.datasets[1].values = oldConsumption;
+  chartDataDayahead.datasets[1].values = oldConsumption;
+  console.log(tempChartData.datasets[1].values); // Debug
+
+  // Add new item to list
+  let node = document.createElement("li");
+  node.appendChild(
+    document.createTextNode(
+      action[action[action.selectedIndex].id].textContent +
+        " " +
+        startTime +
+        "-" +
+        endTime
+    )
+  );
+  node.className = "list-group-item";
+
+  document.getElementById("action-list").appendChild(node);
+
+  console.log("LOL: " + newConsumptionPrices); // dEBUG
 
   // Update chart
   chart.update(tempChartData);
-
+  
   // Calculate electricity consumption price
   let totalPrice = 0;
   let prices = tempChartData.datasets[0].values;
@@ -168,6 +249,7 @@ const updateConsumptionChart = (event) => {
   // Update consumption price
   let consumptionLable = document.getElementById("consumption-price");
   consumptionLable.innerText = totalPrice + "€";
+  
 };
 
 buildChart();
