e8aa602b3f62ee2f45ef91366c8902ab39fd3031
mrMikoma <miko.lj.mattila@gmail.com>
2023-10-23 11:36:55 +0300

    UI update

--- Project/control/codeControl.js
+++ Project/control/codeControl.js
@@ -6,20 +6,77 @@ Used documentation:
 */
 
 // Global variables
-let relayOn = true;
+let relayOn = false;
+
+// Add relay update button
+const relayUpdateButton = document.getElementById("relay-update");
+relayUpdateButton.addEventListener("click", () => {
+  updateRelayPage();
+  getRelayState();
+});
 
 // Add relay control button
-const relayButton = document.getElementById("relay-button");
-relayButton.addEventListener("click", () => {
+const relayToggleButton = document.getElementById("relay-button");
+relayToggleButton.addEventListener("click", () => {
   // Toggle button
   if (relayOn) {
     relayOn = false;
-    relayButton.className = "btn btn-success";
-    relayButton.textContent = "Turn on"
+    relayToggleButton.className = "btn btn-success";
+    relayToggleButton.textContent = "Turn on";
   } else {
     relayOn = true;
-    relayButton.className = "btn btn-danger";
-    relayButton.textContent = "Turn off"
+    relayToggleButton.className = "btn btn-danger";
+    relayToggleButton.textContent = "Turn off";
   }
-  console.log("haha"); // Debug
+  updateRelayPage();
+  getRelayState(); // Debug
 });
+
+// Update relay status on page
+const updateRelayPage = () => {
+  // Get needed elements
+  let statusLabel = document.getElementById("relay-status");
+  let statusInfo = document.getElementById("info-status");
+
+  // Toggle button
+  if (relayOn) {
+    statusLabel.className = "p-3 border border-secondary border-1 bg-success";
+    statusLabel.textContent = "Relay is ON";
+    statusInfo.className = "border border-secondary border-1 status-on";
+  } else {
+    statusLabel.className = "p-3 border border-secondary border-1 bg-danger";
+    statusLabel.textContent = "Relay is OFF";
+    statusInfo.className = "border border-secondary border-1 status-off";
+  }
+
+  // Update timer
+  updateDateTime();
+};
+
+// Get current date and time
+const updateDateTime = () => {
+  // Get needed elements
+  let updateLabel = document.getElementById("update-timer");
+
+  // Get time and date
+  let today = new Date();
+  let timeString =
+    String(today.getHours()).padStart(2, "0") +
+    ":" +
+    String(today.getMinutes()).padStart(2, "0");
+  let dateString =
+    String(today.getDate()).padStart(2, "0") +
+    "." +
+    String(today.getMonth() + 1).padStart(2, "0") +
+    "." +
+    today.getFullYear();
+
+  // Update status
+  console.log("currently: " + dateString + " " + timeString); // Debug
+  updateLabel.textContent = dateString + " " + timeString;
+};
+
+// Get relay state from the device
+const getRelayState = () => {
+  console.log("hello, relay is: " + relayOn); // Debug
+};
